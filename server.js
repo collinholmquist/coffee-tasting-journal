8a7071323b06800636bef145dfad028ebd4e5cc0
Collin Holmquist <collinholmquist@gmail.com>
2021-01-26 23:07:49 -0600

    much done

--- server.js
+++ server.js
@@ -1,19 +1,47 @@
 const express = require('express')
-//const bodyparser = require('body-parser')
+const bodyparser = require('body-parser')
 const app = express()
-//const path = require('path')
+const path = require('path')
+const mysql = require('mysql')
+const dbConfig = require('./dbConfig.js')
+const coffeeConfig = require('./static/coffeeConfig')
+
 
 const port = process.env.PORT || 3000;
 
-//app.use("/public",express.static('public'))
-//app.use(bodyparser.urlencoded({extended: true}))
+app.use("/public",express.static('public'))
+app.use(bodyparser.urlencoded({extended: true}))
 
 app.set("view engine", "ejs")
-//app.set("views", path.join(__dirname, "views/index.ejs"))
 
-app.get('/', (req, res) => {
-    res.render('pages/index')
+//create connection to db
+
+const connection = mysql.createConnection({
+    host: dbConfig.host,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database
 })
 
+connection.connect(error=> {
+    if(error){throw error}
+    console.log('successfully connected to db')
+})
+
+
+
+
+app.get('/', (req, res) => {
+
+    brewmethods = coffeeConfig.brew_methods
+    console.log(brewmethods)
+    taste_profile = coffeeConfig.flavor_profiles
+    console.log(taste_profile)
+
+    res.render('pages/index', {
+        brewmethods: brewmethods,
+        taste_profile: taste_profile
+    })
+})
 
 app.listen(port, () => console.log("Server Up and running"));
---
+++ static/coffeeConfig.js
@@ -0,0 +1,4 @@
+const brew_methods = ['Aeropress', 'Chemex', 'Clever', 'Drip', 'French Press']
+const flavor_profiles = ['Floral', 'Berry', 'Dried Fruit', 'Other Fruit', 'Citrus', 'Brown Spice', 'Nutty', 'Cocoa', 'Sweet', 'Roasted']
+
+module.exports = {brew_methods, flavor_profiles}
