2d935fb47f59f6161714d5c72cf959290da804bd
Collin Holmquist <collinholmquist@gmail.com>
2021-01-27 21:23:57 -0600

    write to mysql

---
+++ models/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql')
+const dbConfig = require('../dbConfig')
+
+const connection = mysql.createConnection({
+    host: dbConfig.host,
+    user: dbConfig.user,
+    password: dbConfig.password,
+    database: dbConfig.database
+})
+
+connection.connect(error=> {
+    if(error){throw error}
+    console.log('successfully connected to db')
+})
+
+module.exports = connection
--- server.js
+++ server.js
@@ -2,10 +2,8 @@ const express = require('express')
 const bodyparser = require('body-parser')
 const app = express()
 const path = require('path')
-const mysql = require('mysql')
-const dbConfig = require('./dbConfig.js')
 const coffeeConfig = require('./static/coffeeConfig')
-
+const Coffee = require('./models/coffee.model')
 
 const port = process.env.PORT || 3000;
 
@@ -16,32 +14,52 @@ app.set("view engine", "ejs")
 
 //create connection to db
 
-const connection = mysql.createConnection({
-    host: dbConfig.host,
-    user: dbConfig.user,
-    password: dbConfig.password,
-    database: dbConfig.database
-})
-
-connection.connect(error=> {
-    if(error){throw error}
-    console.log('successfully connected to db')
-})
-
-
-
 
 app.get('/', (req, res) => {
 
-    brewmethods = coffeeConfig.brew_methods
-    console.log(brewmethods)
-    taste_profile = coffeeConfig.flavor_profiles
-    console.log(taste_profile)
-
     res.render('pages/index', {
-        brewmethods: brewmethods,
-        taste_profile: taste_profile
+        brewmethods: coffeeConfig.brew_methods,
+        taste_profile: coffeeConfig.flavor_profiles
     })
+})
+
+
+
+app.post('/', async (req, res) => {
+
+    
+    try{
+
+        if(!req.body){
+            res.status(400).send({
+                message: 'Content cannot be empty'
+            })
+        }
+    
+        const newCoffee = new Coffee({
+            roaster: req.body.roaster,
+            region: req.body.region,
+            tasting_notes: req.body.tasting_notes.toString(),
+            brew_method: req.body.brew_method,
+            comments: req.body.comments
+        })
+
+        console.log(newCoffee.tasting_notes)
+
+        Coffee.create(newCoffee, (err, data) => {
+            if (err) 
+            res.status(500).send({ message: err.message || "Some error occurred while creating the entry."})
+            else {
+              res.send(data)
+          }
+        })
+    
+
+
+    } catch(error) {
+        res.redirect('/')
+    }
+
 })
 
 app.listen(port, () => console.log("Server Up and running"));
