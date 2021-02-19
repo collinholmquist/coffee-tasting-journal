48713c7d59c12a0cee7bd78c11879f33447fe82c
Collin Holmquist <collinholmquist@gmail.com>
2021-02-18 21:39:08 -0600

    env vars dealt with ... oops

--- dbConfig.js
+++ dbConfig.js
@@ -3,12 +3,12 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 // Get the Host from Environment or use default
-const host = process.env.DB_HOST || 'localhost';
+const host = process.env.DB_HOST
 // Get the User for DB from Environment or use default
-const user = process.env.DB_USER || 'root';
+const user = process.env.DB_USER 
 // Get the Password for DB from Environment or use default
-const password = process.env.DB_PASS || 'oysterbeach31';
+const password = process.env.DB_PASS 
 // Get the Database from Environment or use default
-const database = process.env.DB_DATABASE || 'coffee';
+const database = process.env.DB_DATABASE
 
 module.exports = {host, user, password, database}
