6179b47c6c6aa67108ac5fcc77da7685a549948d
Collin Holmquist <collinholmquist@gmail.com>
2021-03-15 21:14:33 -0500

    second update

--- dbConfig.js
+++ dbConfig.js
@@ -3,12 +3,12 @@ const dotenv = require('dotenv')
 dotenv.config()
 
 // Get the Host from Environment or use default
-const host = process.env.DB_HOST || 'localhost'
+const host = process.env.DB_HOST || 'us-cdbr-east-03.cleardb.com'
 // Get the User for DB from Environment or use default
-const user = process.env.DB_USER || 'root'
+const user = process.env.DB_USER || 'bfba53d28f0f17'
 // Get the Password for DB from Environment or use default
-const password = process.env.DB_PASS || 'oysterbeach31'
+const password = process.env.DB_PASS || 'a4593326'
 // Get the Database from Environment or use default
-const database = process.env.DB_DATABASE || 'coffee'
+const database = process.env.DB_DATABASE || 'heroku_cae293aa3d8239e'
 
 module.exports = {host, user, password, database}
